bcff2d1fb7b8595c51397575e18f9d00a7751562
nguyenhoangthienchi <jayesd3v@gmail.com>
2023-04-28 23:03:59 +0700

    update final project

---
+++ kubectl-apply.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+kubectl apply -f deployment-api-feed.yaml
+kubectl apply -f deployment-api-user.yaml
+kubectl apply -f deployment-frontend.yaml
+kubectl apply -f deployment-reverseproxy.yaml
+
+kubectl apply -f service-api-feed.yaml
+kubectl apply -f service-api-user.yaml
+kubectl apply -f service-frontend.yaml
+kubectl apply -f service-reverseproxy.yaml
+
+pause
